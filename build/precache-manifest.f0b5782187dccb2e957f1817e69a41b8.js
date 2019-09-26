935f1c13016a131ceef0183f7d2d3206654d2ae1
Daniela Ichikawa <danichikawa@gmail.com>
2019-09-26 17:53:02 -0300

    try to deploy

---
+++ build/precache-manifest.f0b5782187dccb2e957f1817e69a41b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561775ec6f8c2abf82bdcbaa3bae49c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6001e5e9491017f1a9bb",
+    "url": "/static/css/main.c64688ce.chunk.css"
+  },
+  {
+    "revision": "e38bb4c529919cd54740",
+    "url": "/static/js/2.3a3e3d46.chunk.js"
+  },
+  {
+    "revision": "6001e5e9491017f1a9bb",
+    "url": "/static/js/main.f20b565d.chunk.js"
+  },
+  {
+    "revision": "82662379a9f976cf4ef4",
+    "url": "/static/js/runtime-main.b768e592.js"
+  }
+]);
